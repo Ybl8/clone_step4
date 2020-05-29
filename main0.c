d4ee22a0621d94a13cff04ab3842c2ce8bd3f237
lvyongbin <lvyongbin11@aliyun.com>
2020-05-29 22:33:38 +0800

    我的提交，(ˇˍˇ) 想～

--- main0.c
+++ main0.c
@@ -5,5 +5,11 @@ int main(){
 		printf("404 Not Found!\n");
 		printf("Please Try Again!\n");
 	}
+	for(i=0;i<202;i++){
+		printf("ping www.google.com\n", );
+		if (i=101){
+			printf("ok,all right,I got it.\n");
+		}
+	}
 	return 0;
 }
